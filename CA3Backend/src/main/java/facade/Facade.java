32e528e17abfb8616b668e76449cc06543ec22ef
Neytrax <cph-rh161@cphbusiness.dk>
2018-11-14 15:04:41 +0100

    UsersRest, facade and mappers

--- CA3Backend/src/main/java/facade/Facade.java
+++ CA3Backend/src/main/java/facade/Facade.java
@@ -6,7 +6,10 @@
 package facade;
 
 import dto.PersonDTO;
+import dto.RoleDTO;
+import dto.UserDTO;
 import entity.User;
+import exceptions.NotFoundException;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -28,26 +31,52 @@ public class Facade {
         return emf.createEntityManager();
     }
 
-    public List<User> getAllUsers() {
+    public List<UserDTO> getAllUsers() throws NotFoundException {
         EntityManager em = getEm();
+        List<UserDTO> list;
         try {
-            TypedQuery<User> tq = em.createQuery("Select u from User u", User.class);
-            return tq.getResultList();
+            TypedQuery<UserDTO> tq = em.createQuery("Select new dto.UserDTO(u) from User u", UserDTO.class);
+            list = tq.getResultList();
         }
         finally {
             em.close();
         }
+        if(list.get(0) == null){
+            throw new NotFoundException("No users could be found");
+        }
+        return list;
     }
 
-    public User getUser(String email) {
+    public UserDTO getUser(String email) throws NotFoundException {
         EntityManager em = getEm();
+        User user = null;
         try {
             em.getTransaction().begin();
-            return null;
+            user = em.find(User.class, email);
         }
         finally {
-
+            em.close();
         }
+        if(user == null){
+            throw new NotFoundException("The user could not be found");
+        }
+        return new UserDTO(user);
+    }
+    
+    public List<RoleDTO> getAllRoles() throws NotFoundException {
+        EntityManager em = getEm();
+        List<RoleDTO> list;
+        try {
+            TypedQuery<RoleDTO> tq = em.createQuery("Select new dto.RoleDTO(r) from Role r", RoleDTO.class);
+            list = tq.getResultList();
+        }
+        finally {
+            em.close();
+        }
+        if(list.get(0) == null){
+            throw new NotFoundException("No roles could be found");
+        }
+        return list;
     }
 
     public static List<PersonDTO> SWAPI(int amount) {
--- CA3Backend/src/main/java/rest/UsersRest.java
+++ CA3Backend/src/main/java/rest/UsersRest.java
@@ -5,12 +5,22 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.UserDTO;
+import exceptions.NotFoundException;
+import facade.Facade;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,21 +32,31 @@ import javax.ws.rs.core.Response;
 @Path("users")
 public class UsersRest {
 
+    private Gson gson;
+    private Facade facade;
+
     @Context
     private UriInfo context;
 
     public UsersRest() {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.facade = new Facade(Persistence.createEntityManagerFactory("pu"));
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllUsers() {
-        return null;
+    public Response getAllUsers() throws NotFoundException {
+        List<UserDTO> users = facade.getAllUsers();
+        String json = gson.toJson(users);
+        return Response.ok().entity(json).build();
     }
 
     @GET
+    @Path("{email}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getUser(String content) {
-        return null;
+    public Response getUser(@PathParam("email") String email) throws NotFoundException {
+        UserDTO userDTO = facade.getUser(email);
+        String json = gson.toJson(userDTO);
+        return Response.ok().entity(json).build();
     }
 }
