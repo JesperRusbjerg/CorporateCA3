7cf16fcbfb8dcb436f5f401f110a1ab71176c056
Neytrax <cph-rh161@cphbusiness.dk>
2018-11-14 15:49:11 +0100

    roles

---
+++ CA3Backend/src/main/java/rest/RolesREST.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.RoleDTO;
+import exceptions.NotFoundException;
+import facade.Facade;
+import java.util.List;
+import javax.persistence.Persistence;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author Rasmus
+ */
+@Path("roles")
+public class RolesREST {
+
+    Gson gson;
+    Facade facade;
+    @Context
+    private UriInfo context;
+
+    public RolesREST() {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.facade = new Facade(Persistence.createEntityManagerFactory("pu"));
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJson() throws NotFoundException {
+        List<RoleDTO> list = facade.getAllRoles();
+        String json = gson.toJson(list);
+        return Response.ok().entity(json).build();
+    }
+}
